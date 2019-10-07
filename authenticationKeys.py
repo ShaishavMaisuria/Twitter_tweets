3111b6d4a7eddd7959c8b954553f00a3433812df
ShaishavMaisuria <47580019+ShaishavMaisuria@users.noreply.github.com>
2019-10-07 10:49:36 -0400

    Update authenticationKeys.py

--- authenticationKeys.py
+++ authenticationKeys.py
@@ -1,29 +1,8 @@
-AccessToken ="3115204826-uPPpuRkct6mrPMAbC1dx8LDTHSWBpHRixd80z2l"
-AccessTokenSecret="aqviM27uVlY4KOxO2LRJCgIwwMS88mhQVzQfmp4Aze0W1"
-ConsumerKey="mRLd5btVEmqEjOnMtWggxVmUU"
-ConsumerSecret="voNEoJbpaBPTpWtCsXx6AV2HhehPJQapvxEI02Ksvx1BQsJUvw"
-'''print("AccessToken")
-AccessToken=input()
-print("AccessTokenSecret")
-AccessTokenSecret=input()
-print("ConsumerKey")
-ConsumerKey=input()
-print(" ConsumerSecret")
-ConsumerSecret=input()'''
+AccessToken =" "
+AccessTokenSecret=" "
+ConsumerKey=" "
+ConsumerSecret=" "
 
-'''
-AccessToken =""
-AccessTokenSecret=""
-ConsumerKey=""
-ConsumerSecret=""
-print("AccessToken")
-AccessToken=input()
-print("AccessTokenSecret")
-AccessTokenSecret=input()
-print("ConsumerKey")
-ConsumerKey=input()
-print(" ConsumerSecret")
-ConsumerSecret=input()'''
 
 
 
